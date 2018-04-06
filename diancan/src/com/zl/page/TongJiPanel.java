ea1ad5651e0be6f9d6041678dfc015f1931570d3
张乐 <925294372@qq.com>
2018-04-06 15:13:13 +0800

    统计功能添加时间控件

--- diancan/src/com/zl/page/TongJiPanel.java
+++ diancan/src/com/zl/page/TongJiPanel.java
@@ -1,15 +1,18 @@
 package com.zl.page;
 
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+
+import java.util.Date;
+
 import javax.swing.JButton;
 import javax.swing.JTextPane;
+import javax.swing.SpinnerDateModel;
 
 public class TongJiPanel extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
 
 	/**
 	 * Create the panel.
@@ -22,10 +25,16 @@ public class TongJiPanel extends JPanel {
 		label.setBounds(62, 41, 54, 15);
 		add(label);
 		
-		textField = new JTextField();
-		textField.setBounds(126, 38, 125, 21);
-		add(textField);
-		textField.setColumns(10);
+		SpinnerDateModel model = new SpinnerDateModel();
+		// 获得JSPinner对象
+		JSpinner year = new JSpinner(model);
+		year.setValue(new Date());
+		// 设置时间格式
+		JSpinner.DateEditor editor = new JSpinner.DateEditor(year, "yyyy-MM-dd");
+		year.setEditor(editor);
+		
+		year.setBounds(126, 38, 125, 21);
+		add(year);
 		
 		JButton button = new JButton("\u6309\u65E5\u671F\u67E5\u8BE2");
 		button.setBounds(279, 37, 93, 23);
@@ -35,10 +44,16 @@ public class TongJiPanel extends JPanel {
 		label_1.setBounds(62, 72, 54, 15);
 		add(label_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(126, 69, 125, 21);
-		add(textField_1);
-		textField_1.setColumns(10);
+		SpinnerDateModel model2 = new SpinnerDateModel();
+		// 获得JSPinner对象
+		JSpinner year2 = new JSpinner(model2);
+		year2.setValue(new Date());
+		// 设置时间格式
+		JSpinner.DateEditor editor2 = new JSpinner.DateEditor(year2, "yyyy-MM");
+		year2.setEditor(editor2);
+		
+		year2.setBounds(126, 69, 125, 21);
+		add(year2);
 		
 		JButton button_1 = new JButton("\u6309\u6708\u4EFD\u67E5\u8BE2");
 		button_1.setBounds(279, 68, 93, 23);
